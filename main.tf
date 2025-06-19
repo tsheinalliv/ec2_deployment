5de72aa98d86091bc8eb9c2adaf95ccd4f42f284
tsheinalliv <64655595+tsheinalliv@users.noreply.github.com>
2025-06-19 16:40:43 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "hcp-ec2-demo"
+  }
+}
